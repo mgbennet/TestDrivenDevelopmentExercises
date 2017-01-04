6f56ee6964767d8124c5e99ffbdbdaa18dfc7efe
mgbennet <mgbennet@gmail.com>
2017-01-03 16:35:57 -0800

    initial commit

---
+++ superlists/functional_tests.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+
+binary = FirefoxBinary("C:/Program Files (x86)/Mozilla Firefox/firefox.exe")
+browser = webdriver.Firefox(firefox_binary=binary)
+browser.get('http://localhost:8000')
+
+assert 'Django' in browser.title
