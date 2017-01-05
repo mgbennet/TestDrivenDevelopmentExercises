c644829c150d363cbb287e28ebe28ca4c643efd4
mgbennet <mgbennet@gmail.com>
2017-01-04 16:50:37 -0800

    Added readme, FT uses unittest now.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Test Driven Development workspace
+Just a place to put the excercises while I work through [Test-Driven Development with Python](http://chimera.labs.oreilly.com/books/1234000000754/index.html) by Harry Percival.
--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,8 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+import unittest
 
-binary = FirefoxBinary("C:/Program Files (x86)/Mozilla Firefox/firefox.exe")
-browser = webdriver.Firefox(firefox_binary=binary)
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        binary = FirefoxBinary("C:/Program Files (x86)/Mozilla Firefox/firefox.exe")
+        self.browser = webdriver.Firefox(firefox_binary=binary)
+        self.browser.implicitly_wait(3)
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_list_and_retrieve_it_later(self):
+        self.browser.get("http://localhost:8000")
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail("Finish the test!")
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
