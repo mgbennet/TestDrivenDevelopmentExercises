c347104c0d36e0b1f1102bfe220dddd6973d8eb6
mgbennet <mgbennet@gmail.com>
2017-01-07 22:27:43 -0800

    Redirect after post, shows all items in template. Still fails functional test, though.

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -32,12 +32,14 @@ class NewVisitorTest(unittest.TestCase):
         )
         inputbox.send_keys("Buy peacock feathers")
         inputbox.send_keys(Keys.ENTER)
+        self.browser.implicitly_wait(5)
         self.check_for_row_in_list_table("1. Buy peacock feathers")
 
         inputbox = self.browser.find_element_by_id("id_new_item")
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
 
+        self.browser.implicitly_wait(5)
         self.check_for_row_in_list_table("1. Buy peacock feathers")
         self.check_for_row_in_list_table("2. Use peacock feathers to make a fly")
 
