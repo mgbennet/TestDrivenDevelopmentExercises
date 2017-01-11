cfbe682ebf697f6eb40977561d2d6414f8d48674
mgbennet <mgbennet@gmail.com>
2017-01-10 22:05:31 -0800

    Fixed functional test error that I think came from Selenium3

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,5 +1,8 @@
+from contextlib import contextmanager
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 import unittest
 
@@ -12,6 +15,13 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    @contextmanager
+    def wait_for_page_load(self, timeout=30):
+        old_page = self.browser.find_element_by_tag_name("html")
+        yield WebDriverWait(self.browser, timeout).until(
+            EC.staleness_of(old_page)
+        )
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id("id_list_table")
@@ -32,16 +42,17 @@ class NewVisitorTest(unittest.TestCase):
         )
         inputbox.send_keys("Buy peacock feathers")
         inputbox.send_keys(Keys.ENTER)
-        self.browser.implicitly_wait(5)
-        self.check_for_row_in_list_table("1. Buy peacock feathers")
+
+        with self.wait_for_page_load(timeout=10):
+            self.check_for_row_in_list_table("1: Buy peacock feathers")
 
         inputbox = self.browser.find_element_by_id("id_new_item")
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
 
-        self.browser.implicitly_wait(5)
-        self.check_for_row_in_list_table("1. Buy peacock feathers")
-        self.check_for_row_in_list_table("2. Use peacock feathers to make a fly")
+        with self.wait_for_page_load(timeout=10):
+            self.check_for_row_in_list_table("1: Buy peacock feathers")
+            self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
         self.fail("Finish the test!")
 
